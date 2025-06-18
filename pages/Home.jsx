ba484068cd01c43fe09adf09d955ecb588a68478
Tomer1994Ab <Abarbanel1994@gmail.com>
2025-06-18 11:53:16 +0300

    Adjust car states to book states

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,7 +3,7 @@ export function Home() {
 
     return (
         <section className="home container">
-            <h1 >Car's R Us!</h1>
+            <h1 >Book's R Us!</h1>
             <img src="../assets/img/react.png" alt="hero-image" />
         </section>
     )
